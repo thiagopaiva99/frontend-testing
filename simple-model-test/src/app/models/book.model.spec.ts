ded77dafc3ba5d3ceeb744225e32874d53eb4cc8
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-12 22:55:55 -0200

    Creating the save, find and delte methods on book model and preparing fake local storage on Book spec

--- simple-model-test/src/app/models/book.model.spec.ts
+++ simple-model-test/src/app/models/book.model.spec.ts
@@ -8,22 +8,48 @@ describe('Book Model', () => {
     let price: number;
     let upvotes: number;
 
+    let book: Book;
+
     beforeEach(() => {
         image = faker.image.image();
         title = faker.lorem.words();
         description = faker.lorem.sentence();
         price = faker.commerce.price();
         upvotes = faker.random.number();
+
+        book = new Book(image, title, description, price, upvotes);
+
+        let storage = {};
+
+        spyOn(window.localStorage, 'getItem').and.callFake((key: string): string => {
+            return storage[key] || null;
+        });
+
+        spyOn(window.localStorage, 'removeItem').and.callFake((key: string): void => {
+            delete storage[key];
+        });
+
+        spyOn(window.localStorage, 'setItem').and.callFake((key: string, value: string): string => {
+            return storage[key] = value;
+        });
+
+        spyOn(window.localStorage, 'clear').and.callFake((): void => {
+            storage = {};
+        });
     });
 
-    it ('should has a valid model', () => {
-        let book = new Book(image, title, description, price, upvotes);
+    afterEach(() => {
+        localStorage.clear();
+    })
 
-        expect(book).toBeTruthy();
-        expect(book.image).toEqual(image);
-        expect(book.title).toEqual(title);
-        expect(book.description).toEqual(description);
-        expect(book.price).toEqual(price);
-        expect(book.upvotes).toEqual(upvotes);
+    it ('should has a valid model', () => {
+        let newBook = new Book(image, title, description, price, upvotes);
+
+        expect(newBook).toBeTruthy();
+        expect(newBook.image).toEqual(image);
+        expect(newBook.title).toEqual(title);
+        expect(newBook.description).toEqual(description);
+        expect(newBook.price).toEqual(price);
+        expect(newBook.upvotes).toEqual(upvotes);
     });
 });
--- simple-model-test/src/app/models/book.model.ts
+++ simple-model-test/src/app/models/book.model.ts
@@ -13,7 +13,53 @@ export class Book implements BookInterface {
         public description: string,
         public price: number,
         public upvotes: number = 0
-    ) {
+    ) { }
 
+    destroy() {
+        let books: Array<Book> = JSON.parse(localStorage.getItem('books') || '[]');
+
+        books.forEach((book, index) => {
+            if (book.title === this.title) {
+                books.splice(index, 1);
+
+                localStorage.setItem('books', JSON.stringify(books));
+            }
+        });
+
+        return null;
+    }
+
+    save() {
+        let books: Array<Book> = JSON.parse(localStorage.getItem('books') || '[]');
+
+        books.forEach((book, index) => {
+            if (book.title === this.title) {
+                books.splice(index, 1);
+            }
+        })
+
+        books.push(this);
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+        return true;
+    }
+
+    find(title: string) {
+        let books: Array<Book> = JSON.parse(localStorage.getItem('books') || '[]');
+
+        books.map(book => {
+            if (book.title === title) {
+                return new Book(
+                    book.image,
+                    book.title,
+                    book.description,
+                    book.price,
+                    book.upvotes
+                );
+            }
+
+            return null;
+        })
     }
 }

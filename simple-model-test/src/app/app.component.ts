cecaf36d09420fb218bc2c41044aa03c91d6fe04
Thiago Galvani <thiagopaiva99@gmail.com>
2019-01-12 22:05:38 -0200

    Adding the BookComponent to AppComponent spec

--- simple-model-test/src/app/app.component.ts
+++ simple-model-test/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Book } from './models/book.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simple-model-test';
+  public title: string = 'simple-model-test';
+
+  public book: Book = new Book(
+    'https://images.livrariasaraiva.com.br/imagemnet/imagem.aspx/?pro_id=8522135&qld=90&l=430&a=-1',
+    'Uma Breve História do Tempo',
+    'Uma das mentes mais geniais do mundo moderno, Stephen Hawking guia o leitor na busca por respostas a algumas das maiores dúvidas da humanidade: Qual a origem do universo? Ele é infinito? E o tempo? Houve um começo e haverá um fim? Existem outras dimensões além das três espaciais? E o que vai acontecer quando tudo terminar?',
+    35,
+    0
+  );
+
 }
